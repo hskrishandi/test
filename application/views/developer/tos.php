2f1bed97d39eb6d02074e06eee46f9d4edb17bfc
Man Ting Wong <Man Ting Wong>
2016-04-15 22:41:51 +0800

    newui Update

--- application/views/developer/tos.php
+++ application/views/developer/tos.php
@@ -1,4 +1,4 @@
-<?php extend('layout.php'); ?>
+<?php extend('layouts/layout.php'); ?>
 
 	<?php startblock('title'); ?>
 		Developer
@@ -10,18 +10,12 @@
     <?php endblock(); ?>
 	
 	
-	
+	<?php startblock('banner'); ?>
+	<img id="slideImage" class="slide" src="<?php echo resource_url('img', 'develop/developerslider.png'); ?>" />
+	<?php endblock(); ?>
 	<?php startblock('content'); ?>
 	<div id="developer">
-		<!-- SlideImage-->
-		<div id="slideBox" class="clearfix">
-			<img id="slideImage" class="slide" src="<?php echo resource_url('img', 'develop/developerslider.jpg'); ?>" />
-			<p id="slideText">
-				Your<br/>Developer <br/>Model<br/>
-			</p>
-			<img id="slideOverlay" class="slide"src="<?php echo resource_url('img', 'develop/slideOverlay.svg'); ?>" />
-		</div>
-		<!--SlideImage End-->
+		
 		<div  id="tos-page">
 			<form action="" method="post" enctype="multipart/form-data">
 				
@@ -31,10 +25,10 @@
 					1. You must own all content uploaded to i-MOS without copyright disputation. When you upload a model on i-MOS, you are granting us a license to distribute on i-MOS site for other users to run simulation. You can revoke the license with a written notice to the i-MOS team. <br/> <br/>
 					2. We hold the right to modify the model. As the simulation is powered with Ngspice simulation engine while the model is implemented with Verilog-A code. The technique gap between Verilog-A model and Ngspice leads to the necessary. Please be noted that not all model can be implemented in i-MOS although we will try our best.<br/> <br/>
 					3. We hold the right to modify or update the terms without notice. The modifications will be enforced immediately once posted on the agreement. Your further use of the platform will be considered your acceptance of the changes. So please continuously keep on visiting this page to keep up with the modifications.
-					<div class="buttons">
+					
 					<input class="submit" type="submit" name="response" value="Cancel" />
 					<input class="submit" type="submit" name="response" value="I Agree" />
-				</div>
+			
 				</div>
 				
 			</form>
--- application/views/news_event/news.php
+++ application/views/news_event/news.php
@@ -1,66 +1,90 @@
-<?php extend('resources/layout.php'); ?>	
-	<?php startblock('title'); ?>
-		News
-	<?php endblock(); ?>	
-	
-	<?php startblock('content'); ?>
-		<div id="resources">
-			<div id="resource-content">
-            <?php if($display_list):?>
-				<div class="title">News
-					<a class="return-link" href="<?php echo base_url('news_event'); ?>" onclick="history.go(-1); return false;">
-							Back to News and Event
-					</a>
-					<div class="postSeparateLine">
-					&#x7c;
-					</div>
+<!--edited by Grace-->
+<?php extend('layouts/layout.php'); ?>	
+<?php startblock('title'); ?>
+News
+<?php endblock(); ?>
 
-					<a class="post" href="<?php echo base_url('resources/post/news'); ?>">Post News</a>
-				</div>
-                
-				<ul class="item-list">
-					<?php foreach($news as $entry): ?>
-					<li> 
-                    	<?php 
-							echo '<a href="' . base_url('news_event/news/' . $entry->id) . '" class="entryTitle"  >' . $entry->title . '</a>';
-						?>
-                    	<span class="date"> <?php echo date('d M Y', $entry->post_date); ?> </span>
-					</li>
-					<?php endforeach; ?>
-                 </ul>
-                
-                    <?php else:?>
-                    <div class="title">News 
-                    	<a class="return-link" href="<?php echo base_url('news_event'); ?>" onclick="history.go(-1); return false;">
-							Back to News Page
-						</a>
-                    </div>
-                    <div class="details">
-                    	<?php foreach($news_details as $entry): ?>
-   
-                        
-                    	<div class="newsTitle"><?php echo $entry->title;?></div>
-                    	<br/>
-                  		<span class="date"><?php echo date('d M Y', $entry->post_date); ?></span>
-                  		<br/>
-                        <div><?php echo nl2br($entry->content);?></div>
-	      				<br /><br />
-	      				<?php if($entry->source_link != "") { ?>
-	      				Source: <a href="<?php echo $entry->source_link; ?>" target="_blank"><?php echo $entry->source_link; ?></a>
-	      				<?php } ?>
-	      				<br/><br/>
-      					<a  href="<?php echo base_url('news_event'); ?>" onclick="history.go(-1); return false;">
-							< Back to News Page
-						</a>
-        
-                        
-                    	<?php endforeach; ?>
-                    	</div
-                    <?php endif;?>
-				
-                 
-			</div>
+<?php startblock('css'); ?>
+<?php echo get_extended_block(); ?>
+<link rel="stylesheet" type="text/css" href="<?php echo resource_url('css', 'news_event.css'); ?>" media="all" />
+<?php endblock(); ?>
+
+<?php startblock('script'); ?>
+<?php echo get_extended_block(); ?>
+<script type="text/javascript" src="<?php echo resource_url('js', 'library/jquery.validate.min.js');?>"></script>
+<script type="text/javascript" src="<?php echo resource_url('js', 'resources.js'); ?>"></script>
+<script type="text/javascript" src="<?php echo resource_url('js', 'calendarDateInput.js'); ?>">
+
+/***********************************************
+* Jason's Date Input Calendar- By Jason Moon http://calendar.moonscript.com/dateinput.cfm
+* Script featured on and available at http://www.dynamicdrive.com
+* Keep this notice intact for use.
+***********************************************/
+
+</script>
+<?php endblock(); ?>
+<?php startblock('banner'); ?>
+<img id="slideImage" class="slide" src="<?php echo resource_url('img', 'news/news.png'); ?>" />
+<?php endblock(); ?>
+<?php startblock('content'); ?>
+<div id="resources">
+
+	<?php if($display_list):?>
+	<div class="page-subtitle">News
+		<a href="<?php echo base_url('news_event'); ?>" onclick="history.go(-1); return false;">
+			Back to News and Event
+		</a>
+		<div class="postSeparateLine">
+			&#x7c;
 		</div>
-	<?php endblock(); ?>
+
+		<a href="<?php echo base_url('resources/post/news'); ?>">Post News</a>
+	</div>
+
+	<ul class="item-list">
+		<?php foreach($news as $entry): ?>
+		<li class="item"> 
+			<?php 
+			echo '<a href="' . base_url('news_event/news/' . $entry->id) . '" class="entryTitle"  >' . $entry->title . '</a>';
+			?>
+			<span class="date"> <?php echo date('d M Y', $entry->post_date); ?> </span>
+		</li>
+	<?php endforeach; ?>
+
+</ul>
+
+<?php else:?>
+	<div class="page-subtitle">News 
+		<a href="<?php echo base_url('news_event'); ?>" onclick="history.go(-1); return false;">
+			Back to News Page
+		</a>
+	</div>
+	<div class="details">
+		<?php foreach($news_details as $entry): ?>
+
+
+		<div class="newsTitle"><?php echo $entry->title;?></div>
+		<br/>
+		<span class="date"><?php echo date('d M Y', $entry->post_date); ?></span>
+		<br/>
+		<div><?php echo nl2br($entry->content);?></div>
+		<br /><br />
+		<?php if($entry->source_link != "") { ?>
+		Source: <a href="<?php echo $entry->source_link; ?>" target="_blank"><?php echo $entry->source_link; ?></a>
+		<?php } ?>
+		<br/><br/>
+		<a  href="<?php echo base_url('news_event'); ?>" onclick="history.go(-1); return false;">
+			< Back to News Page
+		</a>
+
+
+	<?php endforeach; ?>
+</div>
+<?php endif;?>
+
+
+
+</div>
+<?php endblock(); ?>
 
 <?php end_extend(); ?>
