33618df264d0d266578c37232f29f9fc6a513d96
Tony Chu <Tony Chu>
2016-04-16 12:15:24 +0800

    applying new layout

--- application/views/resources/groups.php
+++ application/views/resources/groups.php
@@ -8,11 +8,11 @@
 	
 	<?php startblock('content'); ?>
 		<div id="resources">		
-			<p id="pageTitle">Resources</p>
-				<div class="title">Organizations
+			<p class="page-title">Resources</p>
+				<div class="page-subtitle">Organizations
 					<div class="postSeparateLine">
 					</div>
-					<a class="post" href="<?php echo base_url('resources/post/groups'); ?>">Post Organization</a>
+					<a class="page-subtitle-more" href="<?php echo base_url('resources/post/groups'); ?>">Post Organization</a>
 				</div>
 				<ul class="item-list">
 					<?php foreach($groups as $entry): ?>
