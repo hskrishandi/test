7b02a0787c235e9e4407a7b1abaf10864f4f55fa
leon <leon390422914@gmail.com>
2016-05-19 16:49:20 +0800

    fix font weight problem and add some files

---
+++ application/views/simulation/descriptions/avs100.php
@@ -0,0 +1,41 @@
+<h2>An Ambipolar Virtual-Source-Based Charge-Current CompactModel for Graphene
+Field-Effect Transistors</sup></h2>
+<p>Authors:Shaloo Rakheja
+<br>Organization: MIT
+
+<h4>Introduction</h4>
+<div class="details">
+        <div class="structure-figure">
+         <img src="<?php echo base_url('images/simulation/avs100.png');?>" />              
+        </div>
+        <p>Most of the previous transport (I-V ) modeling efforts in GFETs have relied on the drift-diffusion (DD) theory of electron transport with density-dependent saturation velocity [1], [2], [3], [4], [5]. As such the validity of DD-based models may be limited to long channel devices.
+ </p>
+  <p>
+    At MIT, an alternate transport model based on the concept of virtual source (VS) charge/velocity has been developed for GFETs in both unipolar and ambipolar regimes of operation. The model is also supplemented with channel charge partitioning that is valid from the drift diffuive to the ballistic transport regimes, where the gradual channel approximation (GCA) is no longer valid.
+    </p>
+<p>
+The figure in right side shows a GFET with its terminal voltages labeled as d (drain), g (gate),and s (source). The internal and external gate terminals are the same, while the internal drain and source terminals are labeled as di and si, respectively. Also labeled are the source and the drain channel-access resistances as Rs and Rd,respectively.
+</p>
+</div>
+<h4>References</h4>
+<div class="reference clear">
+         <p>
+      [1] G. Zebrev, A. Tselykovskiy, D. Batmanova, and E. Melnik,"Small-signal capacitance and current parameter modeling in large-scale high-frequency graphene field-effect transistors," Electron Devices, IEEE Transactions on,vol. 60, no. 6, pp. 1799-1806, 2013.
+</p>
+<p>
+     [2] I. Meric, C. Dean, A. Young, J. Hone, P. Kim, and K. L. Shepard, "Graphene field-effect transistors based on boron nitride gate dielectrics," in Electron DevicesMeeting (IEDM), 2010 IEEE International, 2010, pp. 23.2.1-23.2.4.
+</p>
+<p>
+     [3] S. Thiele, J. Schaefer, and F. Schwierz, "Modeling of graphene metal-oxidesemiconductor field-effect transistors with gapless large-area graphene channels," Journal of Applied Physics, vol. 107, no. 9, pp. 094 505-094 505-8,2010.
+</p>
+<p>
+     [4] D. Jiminez and O. Moldovan, "Explicit drain-current model of graphene field-effect transistors targeting analog and radio-frequency applications," IEEE Transactions on Electron Devices, vol. 58, no. 11, pp. 4049-4052, November 2011.
+</p>
+<p>
+     [5] K. Parrrish, M. E. Ramon, S. K. Banerjee, and D. Akinwande, "A compact model for graphene fets for linear and non linear circuits," in Simulation of Semiconductor Processes and Devices (SISPAD), 2012 International Conference on, Denver, Colorado, September 2012.
+
+
+        </p>
+        </div>
+
+
