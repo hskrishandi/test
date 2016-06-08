ba42e93d1d058d5d8f874d8e70bca8516d667fa0
Alex Lam <eekclam@ust.hk>
2016-06-08 11:29:29 +0800

    Source links from nanoHUb added

--- application/views/simulation/descriptions/mvshemt120.php
+++ application/views/simulation/descriptions/mvshemt120.php
@@ -1,7 +1,8 @@
-
 <h2>Silicon MIT virtual source HEMT model</sup></h2>
 <p>Authors:Ognian Shaloo Rakheja,Dimitri Antoniadis
 <br>Organization: Massachusetts Institute of Technology (MIT)
+<br>Source code: <a href="https://nanohub.org/publications/71/1" target="_blank$">
+      MIT Virtual source(MVS) HEMT model</a>
 <h4>Introduction</h4>
 <div class="details">
         <div class="structure-figure">
