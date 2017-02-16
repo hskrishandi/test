075c8dad93f91b91bc4df39d874c526757d37714
Leon <hliangae@ust.hk>
2017-02-16 18:07:44 +0800

    add reset password; add random_compat library to generate strong secure password; change auth_repository/auth_service to extends base_repository/base_service

--- application/libraries/password.php
+++ application/libraries/password.php
@@ -6,12 +6,18 @@ if (! defined('BASEPATH')) {
 
 class Password
 {
+    // Don't rename this, this name will be used in password.inc.
+    // And this variable holds plain text password,
+    // use encrypt function to get the
+    // encrypted password.
     public $pass;
 
     public function __construct($password = null)
     {
-		require_once dirname(__FILE__) . '/bootstrap.inc';
+        require_once dirname(__FILE__) . '/bootstrap.inc';
         require_once dirname(__FILE__) . '/password.inc';
+        // For generating secure random password
+        require_once dirname(__FILE__) . '/random_compat/lib/random.php';
         $this->pass = $password;
     }
 
@@ -45,5 +51,36 @@ class Password
             $this->pass = $password;
         }
         return _password_crypt('sha512', $this->pass, _password_generate_salt(DRUPAL_HASH_COUNT));
+    }
+
+    /**
+     * Generate strong random passowrd
+     *
+     * @param $length - should be even number like 6, 8, 10...
+     * @return $password
+     *
+     * @author Leon
+     */
+    public function generate($length)
+    {
+        $randomBytes = null;
+        try {
+            // Generate random string using random_compact library
+            $randomBytes = random_bytes($length / 2);
+        } catch (TypeError $e) {
+            // Well, it's an integer, so this IS unexpected.
+            log_message('error', $e);
+        } catch (Error $e) {
+            // This is also unexpected because 0 and 255 are both reasonable integers.
+            log_message('error', $e);
+        } catch (Exception $e) {
+            // If you get this message, the CSPRNG failed hard.
+            log_message('error', $e);
+        }
+        if ($randomBytes) {
+            // Change string to hex code
+            $this->pass = bin2hex($randomBytes);
+        }
+        return $this->pass;
     }
 }
