f4a255c97351e10ccaa1ff63a1434a4dff6f75fb
Leon <hliangae@ust.hk>
2017-07-27 23:36:29 +0800

    reload userlibrary when upload library finished

--- js/realcas/imos-addon.js
+++ js/realcas/imos-addon.js
@@ -344,7 +344,7 @@ $(function() {
     $(".model-library-upload").fileupload({
         name: "file",
         url: ROOT + "/modelLibrary/UPLOAD",
-        success: function(data) {
+        load: function(data) {
             if (data.success)
                 viewModels.lib.load();
             else
